2419a864dfea70dbc60ac9bd30c9c6ecc11bdb67
Dominik Windisch <dominik.windisch@students.fhv.at>
2016-06-29 18:31:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var roleHarvester = require('role.harvester');
+//var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
