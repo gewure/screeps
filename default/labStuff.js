4fe03bf3262b1311ed6cea6abdb960580c8995d9
gewure <jasperreichardt@gmail.com>
2016-08-03 21:41:10 +0200

    bla

---
+++ default/labStuff.js
@@ -0,0 +1,11 @@
+if(Find labs without energy) {
+	fill creep with energy
+	fill labs without energy
+} else if(Find labs without resources) {
+	which lab needs what?
+	find what
+	fill lab
+} else {
+   //container with energy 
+  // product -> storage .. 50%?
+}
